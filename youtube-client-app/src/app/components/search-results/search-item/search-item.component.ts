e542708fd1725ebef1775fe38f3ab771598d4b51
Sveta Rachok <sveta.r1212@gmail.com>
2022-10-06 17:59:39 +0300

    feat: add colored background to cards, that changes according to date in card

--- youtube-client-app/src/app/components/search-results/search-item/search-item.component.ts
+++ youtube-client-app/src/app/components/search-results/search-item/search-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { SearchItemInterface } from 'src/app/models/search-item.model';
+import { DateMarks, nowInSeconds } from '../../../utils/dateMarks';
 
 @Component({
   selector: 'app-search-item',
@@ -9,14 +10,38 @@ import { SearchItemInterface } from 'src/app/models/search-item.model';
 export class SearchItemComponent implements OnInit {
   @Input() searchItem!: SearchItemInterface;
 
+  color: string = '';
+
   // eslint-disable-next-line @typescript-eslint/no-useless-constructor, @typescript-eslint/no-empty-function
   constructor() {
-    console.log(this.searchItem);
+    
   }
 
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit(): void {
-    console.log(this.searchItem);
+    this.color = this.setBgColor();
+  }
+
+  setBgColor(): string {
+    const postData = new Date(this.searchItem.snippet.publishedAt);
+    console.log(postData);
+    if (postData <= new Date(nowInSeconds) && postData > new Date(DateMarks.upTo7)) {
+      this.color = 'blue';
+    } 
+    
+    if (postData <= new Date(DateMarks.upTo7) && postData > new Date(DateMarks.upToMonth)) {
+      this.color = 'green';
+    } 
+    
+    if (postData <= new Date(DateMarks.upToMonth) && postData > new Date(DateMarks.upTo6Month)) {
+      this.color = 'yellow';
+    } 
+    
+    if (postData <= new Date(DateMarks.upTo6Month)) {
+      this.color = 'red';
+    }
+
+    return this.color;
   }
 
 }
